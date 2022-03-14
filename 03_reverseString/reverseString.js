a961b718cc0896823962bf3976eb001ec760c73a
eddybayes <orangejamface@gmail.com>
2022-03-14 12:12:00 -0400

    completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    // let stringArray = Array.from(word);
+    // stringArray.reverse();
+    // let newString = stringArray.join('');
+    // return newString;
+
+    // MORE EFFIECIENT BELOW
+     return word.split('').reverse().join('');
 };
 
+// first define the legnth of the string
+// us a loop to iterate through string chracters and return each one individually on its own variable
+// then reorder characters so that they are in reverse, a string
+// return new string
+
+// create array from string, each item in array should be a single character
+// re-order items in the array so that it has been reversed
+// truen array back into string
+// retrun new string
 // Do not edit below this line
 module.exports = reverseString;
