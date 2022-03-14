38c52bf6fe4b0f9ca64c4fbc74a55e1cfb4644af
eddybayes <orangejamface@gmail.com>
2022-03-14 12:12:35 -0400

    completed repeatstring and reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0 )   { 
+        return 'ERROR'
+    }
+    let returnWord = '';
+    for (i=number; i>0; i--)  {
+        returnWord += word;
+    }
+return returnWord;  
 };
 
+
+// First need to 
 // Do not edit below this line
 module.exports = repeatString;
