a591b004d3146ba97d635110d73c1336819e9a5a
eddybayes <orangejamface@gmail.com>
2022-03-15 16:51:01 -0400

    completed 04_removeFromArray and 05_sumAll of java exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,66 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+//   // the very first item in our list of arguments is the array, we pull it out with args[0]
+  const array = args[0];
+//   // create a new empty array
+  const newArray = [];
+//   // use forEach to go through the array
+  array.forEach( function(element) {
+//     // push every element into the new array
+//     // UNLESS it is included in the function arguments
+//     // so we create a new array with every item, except those that should be removed
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
+  // const removeFromArray = function(target,item) {
+
+    // let array = [...target];
+    // let removeIndex = array.indexOf(item);
+
+    //   if (removeIndex > 1) {
+    //     array.splice(removeIndex,1);
+    //   };
+    // return array;
+  
+    // THE ABOVE FUNCTION only removes the first data input in the parameter item
+  // };
+
+
+  // const removeFromArray = function(target,item) {
+
+  //   let array = [...target];
+  //   let toRemove = Array.from(item);
+  //   const toGo = new Set(toRemove);
+
+  //   const newArray = array.filter( (data) => {
+  //     return !toGo.has(data);
+  //   })
+  //   return newArray;
+  // };
+
+  // and return that array
+
+
+// define two paramateres within the function, one is the overall item, one is what needs to be removed from item
+// item will alwasy be index 0, everyting else is index 1 or above
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+// const removeFromArray = function(...args) {
+//     const array = args[0];
+//     const newArray = [];
+
+//     array.forEach( function(element) {
+
+//       if (!args.includes(element)) {
+//         newArray.push(element);
+//       }
+//     });
+//     return newArray;
+//   };
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    // define variables for max number, min number, result at 0 to start, and an array of all arguments in the funtion.
+    let max = Math.max(...args);
+    let min = Math.min(...args);
+    let arrayOfArgs = [...args];
+    let result = 0;
 
+    // if min is less than or equal to 0, return 'ERROR'
+if (min <= 0)    {
+    return 'ERROR';
+}
+
+// run a loop that adds numbers from min to max (includinug min and max), 
+//do this by adding min to itself whilst it increases with each loop
+// loop will compare i to max, if i is smaller than max, run loop until,  start i at 0 and increase i with every loop
+for (i=0; i< max; i++)  {
+    result += min;
+    min++; 
+}
+
+//run .forEach on my array of arguements, if any of them are not a nuumber, return 'ERROR'
+arrayOfArgs.forEach( function(item) {
+    if (typeof item !== 'number') {
+        result = 'ERROR';
+    }
+});
+return result;
 };
 
+// define varibales, one that contains min number and another contains
 // Do not edit below this line
 module.exports = sumAll;
