7f7b8bb02e9f87d6798001d7ada3b8f968a3841f
eddybayes <orangejamface@gmail.com>
2022-03-16 16:06:58 -0400

    solved leapYears, in  a long and clunky way, but i learn from the solution on odin project after I solved my way

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,39 @@
-const leapYears = function() {
 
+
+/// my code is a little clunky and long
+const leapYears = function(entry) {
+
+    if (entry % 4 == 0) {
+        if (entry % 100 == 0) {
+            if (entry % 400 == 0) {
+                return true;
+            }
+            else {return false};
+        }
+        else {return true};
+    }
+    else {
+        return false;
+    }
 };
+
+// more effiecient!!! I see how this is better
+// bascially it returns true is it first is divisible by four and then if it 
+// either is not divisible by 100 or is by 400
+// anything else is false
+
+
+const leapYears = function(year) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+  };
+  
+
+// test entry against the following using an if conditional
+// lets try using the remainder operator
+// divisible by 4 = true
+// divisible by 100 = false
+// divisible by 400 = true
+// so it will have to pass three stages 
 
 // Do not edit below this line
 module.exports = leapYears;
