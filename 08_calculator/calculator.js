8d57864a559b473a4bca66117fe40714fd1fff0f
eddybayes <orangejamface@gmail.com>
2022-05-17 19:59:32 -0400

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for( each of array) {
+    sum += each;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = array.reduce((total,each) => {
+    return total * each;
+  },1);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let sum = 1;
+  for (i=a; i>0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
