b5dc3deea74999ff49a34832bf68814be518136f
eddybayes <orangejamface@gmail.com>
2022-05-19 13:07:54 -0400

    completed fibonacci, woop!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(amount) {
 
-};
-
+    if (amount < 0) return 'OOPS';
+    
+    let a = 0;
+    let b = 1;
+    for (i = amount; i > 1; i--) {
+        let numHold = b;
+        b = a + b;
+        a = numHold;
+        }
+    return b;
+}
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
