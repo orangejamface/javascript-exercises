5a2c0950f836baa6b69f71d1ea15b325a776e9f7
eddybayes <orangejamface@gmail.com>
2022-05-23 20:16:52 -0400

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+let noSpace = word.toUpperCase()
+                  .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                  .replace(/\s+/g, '');
 
+let reversed = '';
+for (let character of noSpace){
+    reversed = character + reversed;
+    }
+
+if (reversed === noSpace) return true;
+else return false
 };
+   
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//  THE ORIGINAL STEPS TAKEN TO MAKE UPPER CASE, REMOVE PUNC AND SPACE
+// let upper = word.toUpperCase();
+// let noPunc = upper.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+// let noSpace = noPunc.replace(/\s+/g, '');
